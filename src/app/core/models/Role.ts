b1ba7a41efc225aa5bb6094138775a4929455511
o.kaffouh <o.kaffouh@inoser.ma>
2024-06-19 16:19:51 +0100

    refactor: Update image URLs in team and our-equipments components

--- src/app/core/models/Role.ts
+++ src/app/core/models/Role.ts
@@ -1,22 +1,14 @@
-export interface Role{
-    id:number|undefined;
-    name:string;
-    description:string;
-    analytics:Boolean;
-
-  membership:Boolean;
-
-  payments:Boolean; // only if he has membership
-
-  inventory:Boolean;
-
-  training:Boolean;
-
-  settings:Boolean;
-
-  preferences:Boolean;
-
-  manageWebSite:Boolean;
-
-  coach:Boolean;
+export interface Role {
+    id: number | undefined;
+    name: string;
+    description: string;
+    analytics: Boolean;
+    membership: Boolean;
+    payments: Boolean; // only if he has membership
+    inventory: Boolean;
+    training: Boolean;
+    settings: Boolean;
+    preferences: Boolean;
+    manageWebSite: Boolean;
+    coach: Boolean;
 }
