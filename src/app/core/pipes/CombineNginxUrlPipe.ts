988cd98875fccc99e68966690f4f3d2b6d578f1f
o.kaffouh <o.kaffouh@inoser.ma>
2024-06-27 17:50:44 +0100

    refactor: Update CombineNginxUrlPipe to use lowercase string type for profilePicture parameter

--- src/app/core/pipes/CombineNginxUrlPipe.ts
+++ src/app/core/pipes/CombineNginxUrlPipe.ts
@@ -6,7 +6,7 @@ import { environment } from 'src/environments/environment';
 })
 export class CombineNginxUrlPipe implements PipeTransform {
 
-  transform(profilePicture: String): string {
+  transform(profilePicture: string): string {
     const baseUrl = environment.NGINX_URL; // Use the base URL from the environment
     const timeStamp = new Date().getTime();
     return `${baseUrl}/${profilePicture}?t=${timeStamp}`;
