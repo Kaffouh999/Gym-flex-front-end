c812e724bea04e267403fe31303a6312937739c6
o.kaffouh <o.kaffouh@inoser.ma>
2024-07-10 15:49:52 +0100

    refactor: Update footer copyright year and replace icons

--- src/app/webClient/pages/home/home.component.ts
+++ src/app/webClient/pages/home/home.component.ts
@@ -25,8 +25,8 @@ export class HomeComponent implements OnInit {
         animateOut: "fadeOut",
         animateIn: "fadeIn",
         navText: [
-            '<i class="fa fa-angle-left"></i>',
-            '<i class="fa fa-angle-right"></i>',
+            '<i><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAhklEQVR4nO2WOwqAMBAFBz8XEJKb6aktRFt7LfUEsQkiaCrJruAOvCLVQMK+DRjGXymAWlrqgQGYgVJK6oARCDGNhjRIiN2DNLvYJaRZxR6YEtK3WYFOWhpi9qfJ6DNLQ+q51MRe4Kq3VAnllC9A+7lxUi0Q1cpUXRKqa/H6EajOk2EY3DkANGS6e2z3DR0AAAAASUVORK5CYII=" /> </i>',
+            '<i><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAg0lEQVR4nO2WMQqAQAwEB3yHos8R/66lqAiK/iM2ljkt9JImA1uGgZBcDoIgeKa4Y84ALEBlLZY7O9B4iMVaLl5yUbK9yTvgTBR/zfo0cEcm6atcDDJre24hnjSxRatLrdVtRvmakv5Fap3qnFK8pChS8ydzsz4SPTB6nEW8PgJB4M8FSIC6vKYZ4mAAAAAASUVORK5CYII="></i>',
         ],
         smartSpeed: 2000,
         autoHeight: false,
